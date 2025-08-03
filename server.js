bb6bb9eb01cc5e94093efd668fa7f2f66a44c96b
Giiin <zerepgen@gmail.com>
2025-08-03 18:04:04 +0800

    Update server.js
    I put the moment and logger

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const moment = require('moment');
+
 const endpointRead = require('./routes/endpointRead');
 const endpointCreate = require('./routes/endpointCreate');
 const endpointSearch = require('./routes/endpointSearch');
 const endpointUpdate = require('./routes/endpointUpdate');
 const endpointDelete = require('./routes/endpointDelete');
+
+const logger = (req, res, next) => {
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl} : ${moment().format()}`);
+    next();
+};
+
+app.use(logger);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -18,4 +27,5 @@ app.use('/deleteUsers', endpointDelete);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+
 });
